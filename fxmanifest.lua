96621ba38d1a62704b561279d5d850f21ecba005
Ilyes442 <119808847+Ilyes442@users.noreply.github.com>
2024-09-26 15:25:20 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name "Izonesafe"
+description "ZoneSafe"
+author "Ilyes4"
+version "1.0"
+
+
+
+client_scripts {
+	'client/*.lua'
+}
+
+
+
+ui_page 'html/index.html'
+
+files {
+	'html/**',
+}
